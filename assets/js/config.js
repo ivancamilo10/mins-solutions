cfbf3313a90c8621ddb8dc43c4a34f8a036b367e
Ivan Mejia <ivantriana2005@gmail.com>
2026-02-25 20:28:30 -0500

    hecho

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,6 @@ window.MINS_CONFIG = {
   email: "minssolutionsti@gmail.com",
   whatsappNumber: "573006984927",
   city: "Barranquilla, Atlantico, Colombia",
-  portfolioUrl: "docs/portafolio.html",
+  portfolioUrl: "portafolio.html",
   defaultWhatsappMessage: "Hola, vengo desde la web de Mins Solutions TI. Quisiera cotizar un proyecto.",
 };
